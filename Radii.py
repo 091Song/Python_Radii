adf140d16925a6922b96af70dd200696d5299fed
Y. Song <091.song@gmail.com>
2019-02-01 11:46:07 +0900

    Check measurements using differences

--- Radii.py
+++ Radii.py
@@ -44,17 +44,26 @@ Intf = np.zeros((h,w), dtype = imgBW.dtype )
 
 Int0 = np.zeros(h)
 Intb = np.zeros(h)
-Intbub = np.zeros(h) ## temporal - comparison
+Intbt = np.zeros(h) ## temporal - comparison
+
+# for local depth 
+BWdepth = np.zeros(w)
 
 # check initial two values
 Int0[0] = pd.Series(imgBW[0,:]).idxmin()
 Intb[0] = pd.Series(imgBW[0,:]).idxmin()
+
+Int0[1] = pd.Series(imgBW[0,:]).idxmin()
+Intb[1] = pd.Series(imgBW[0,:]).idxmin()
+
 # temporal
-Intbub[0] = Intb[0] 
+Intbt[0] = Intb[0] 
+Intbt[1] = Intb[1] 
 
 # search ranges 
 sr = 100 # search limit
-ub = w-sr # upper boundary
+#ub = w-sr # upper boundary
+ub = 1100 # upper boundary
 lw = sr
 
 # use color depth
@@ -62,13 +71,12 @@ ldep = 100
 
 # check the darkest point at a height i 
 # use differences
-for i in range(1, h):
+for i in range(2, h):
     # initial interpolation
     Int0[i] = pd.Series(imgBW[i,:]).idxmin()
     cdep = imgBW[i,Int0[i]]
     # differences
     diff = np.abs(Int0[i] - Int0[i-1])
-    
     
     # interface positions 
     # Intb[i] = pd.Series(imgBW[i,lw:ub]).idxmin() + lw
@@ -76,14 +84,34 @@ for i in range(1, h):
     
     #if (diff < 0.5 * w ):
     # sort step 1: using difference to 
-    if (diff < 0.5 * w and cdep < ldep):
+    if ( diff < 0.5 * w and cdep < ldep):
         # ignore data below ub
         Intb[i] = pd.Series(imgBW[i,lw:ub]).idxmin() + lw
         
     else:
         Intb[i] = ub #Intb[i-1]
         
-        
+#######
+    # using depth works fine typically
+    # copy local BW depth (i.e. at i)
+    BWdepth = imgBW[i,:]
+    # using difference
+    diffp = Intbt[i-2] - Intbt[i-1]
+    
+    if (diffp < 0) :
+        lim1 = Intbt[i-1] + diffp - sr
+        lim2 = Intbt[i-1] - diffp + sr
+    else:
+        lim1 = Intbt[i-1] - diffp - sr
+        lim2 = Intbt[i-1] + diffp + sr
+    
+    # rearrange limts
+    lim1 = lim1 if lim1 > sr else sr
+    lim2 = lim2 if lim2 < ub else ub
+    
+    
+    Intbt[i] = pd.Series(BWdepth[int(lim1):int(lim2)]).idxmin() + int(lim1)
+    #print(i, lim1, lim2)
     
     '''
     # upper limit
@@ -162,18 +190,19 @@ for i in range(0, h):
 #plt.subplot(2,2,4); plt.plot(Y,Intb)
 
 #plt.show()
-#plt.plot(Y, Int0, 'k', Y,Intb, 'b', Y,Intbub, 'r')
+plt.plot(Y, Int0, 'k', Y,Intb, 'b', Y,Intbt, 'r')
 
-plt.plot(Y, Int0, 'k', Y,Intb, 'b--')
-    
-#plt.plot(Y[0:200], Int0[0:200], 'k', Y[0:200],Intb[0:200], 'r--')
+#plt.plot(Y, Int0, 'k', Y,Intb, 'b--')
+
+chkr = 400
+#plt.plot(Y[0:chkr], Int0[0:chkr], 'k', Y[0:chkr],Intb[0:chkr], 'r--')
 #plt.plot(Y, Intb, 'b')
 plt.show()
 
 plt.ylim(-250,250) #https://plot.ly/matplotlib/axes/
 #plt.plot(Y[1:len(Y)]-0.5, (Intb[1:len(Intb)] - Intb[0:len(Intb)-1]))
-plt.plot(Y[1:201], (Intb[2:202] - Intb[0:200]))
+plt.plot(Y[1:chkr+1], (Intb[2:chkr+2] - Intb[0:chkr]))
 plt.show()
 
-plt.plot(X,imgBW[125,:])
+plt.plot(X,imgBW[100,:], X, BWdepth)
 plt.show()
